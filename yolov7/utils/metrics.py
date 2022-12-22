78eba1ea97f651abe3a14b397f5584000f834c2d
NeroHin <hin1997101@gmail.com>
2022-12-22 16:25:44 +0800

    feat: modify yolo metrics weighted

--- yolov7/utils/metrics.py
+++ yolov7/utils/metrics.py
@@ -11,7 +11,7 @@ from . import general
 
 def fitness(x):
     # Model fitness as a weighted combination of metrics
-    w = [0.0, 0.0, 0.1, 0.9]  # weights for [P, R, mAP@0.5, mAP@0.5:0.95]
+    w = [0.0, 0.0, 0.5, 0.5]  # weights for [P, R, mAP@0.5, mAP@0.5:0.95]
     return (x[:, :4] * w).sum(1)
 
 
